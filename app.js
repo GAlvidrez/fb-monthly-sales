80eec379710b9940763f012a651852eb0e13a0f3
GAlvidrez <alvidrezgiovanni@gmail.com>
2024-05-20 15:37:30 -0700

    formatting

--- app.js
+++ app.js
@@ -7,28 +7,29 @@ let currentPage;
 let filterActive = 1
 
 let sSQL = `SELECT  customer AS Customer
-                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'Alex' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Alex
-                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'frankie' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Frankie
-                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'jorge' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Jorge
-                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'mack' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Mack
-                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'roger' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Roger
-                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'ryan' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Ryan
-                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'ryo' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Ryo
-                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'stan' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Stan
-                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'zach' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Zach
-                    , CONCAT("$",FORMAT(SUM(sales.totalPrice),2,"en_US")) AS Total
+                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'Alex' THEN price ELSE 0 END),2,"en_US")) AS Alex
+                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'frankie' THEN price ELSE 0 END),2,"en_US")) AS Frankie
+                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'jorge' THEN price ELSE 0 END),2,"en_US")) AS Jorge
+                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'mack' THEN price ELSE 0 END),2,"en_US")) AS Mack
+                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'roger' THEN price ELSE 0 END),2,"en_US")) AS Roger
+                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'ryan' THEN price ELSE 0 END),2,"en_US")) AS Ryan
+                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'ryo' THEN price ELSE 0 END),2,"en_US")) AS Ryo
+                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'stan' THEN price ELSE 0 END),2,"en_US")) AS Stan
+                    , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'zach' THEN price ELSE 0 END),2,"en_US")) AS Zach
+                    , CONCAT("$",FORMAT(SUM(price),2,"en_US")) AS Total
             FROM    (
                     SELECT
                         so.salesman,
                         customer.name AS customer,
-                        so.totalPrice,
+                        postsoitem.totalPrice AS price,
                         so.dateCompleted,
                         so.dateIssued,
                         sostatus.name
                     FROM so
-                    INNER JOIN postso ON postso.soId = so.id
-                    INNER JOIN customer ON so.customerId = customer.id
-                    INNER JOIN sostatus ON so.statusId = sostatus.id
+                        INNER JOIN postso ON so.id = postso.soId
+                        INNER JOIN postsoitem ON postso.id = postsoitem.postSOId
+                        INNER JOIN customer ON so.customerId = customer.id
+                        INNER JOIN sostatus ON so.statusId = sostatus.id
                     WHERE postSo.postDate between '${dateRange.fromDate}' AND '${dateRange.toDate}'
                     ) AS sales
             GROUP BY Customer;`
@@ -36,32 +37,32 @@ let sSQL = `SELECT  customer AS Customer
 
 
 let totalSQL = `SELECT  'TOTALS' AS Customer
-                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'Alex' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Alex
-                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'frankie' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Frankie
-                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'jorge' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Jorge
-                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'mack' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Mack
-                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'roger' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Roger
-                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'ryan' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Ryan
-                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'ryo' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Ryo
-                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'stan' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Stan
-                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'zach' THEN sales.totalPrice ELSE 0 END),2,"en_US")) AS Zach
-                        , CONCAT("$",FORMAT(SUM(sales.totalPrice),2,"en_US")) AS Total
+                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'Alex' THEN price ELSE 0 END),2,"en_US")) AS Alex
+                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'frankie' THEN price ELSE 0 END),2,"en_US")) AS Frankie
+                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'jorge' THEN price ELSE 0 END),2,"en_US")) AS Jorge
+                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'mack' THEN price ELSE 0 END),2,"en_US")) AS Mack
+                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'roger' THEN price ELSE 0 END),2,"en_US")) AS Roger
+                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'ryan' THEN price ELSE 0 END),2,"en_US")) AS Ryan
+                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'ryo' THEN price ELSE 0 END),2,"en_US")) AS Ryo
+                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'stan' THEN price ELSE 0 END),2,"en_US")) AS Stan
+                        , CONCAT("$",FORMAT(SUM(CASE WHEN sales.salesman = 'zach' THEN price ELSE 0 END),2,"en_US")) AS Zach
+                        , CONCAT("$",FORMAT(SUM(price),2,"en_US")) AS Total
                 FROM    (
                         SELECT
                             so.salesman,
                             customer.name AS customer,
-                            so.totalPrice,
+                            postsoitem.totalPrice AS price,
                             so.dateCompleted,
                             so.dateIssued,
                             sostatus.name
                         FROM so
-                        INNER JOIN postso ON postso.soId = so.id
-                        INNER JOIN customer ON so.customerId = customer.id
-                        INNER JOIN sostatus ON so.statusId = sostatus.id
+                            INNER JOIN postso ON so.id = postso.soId
+                            INNER JOIN postsoitem ON postso.id = postsoitem.postSOId
+                            INNER JOIN customer ON so.customerId = customer.id
+                            INNER JOIN sostatus ON so.statusId = sostatus.id
                         WHERE postSo.postDate between '${dateRange.fromDate}' AND '${dateRange.toDate}'
-                ) AS sales
-                `
-
+                        ) AS sales
+                    `
 
 
 let sales = JSON.parse(runQuery(sSQL));
